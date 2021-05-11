36dfdf343394146f98be13dd0f68de4171bda2c8
Alvaro Torre Van Cutsem <alvarotvance@gmail.com>
2021-05-11 15:15:36 +0200

    DinoSpawnSystemAlmostDONE

---
+++ Assets/DinoGameStuff/ObstacleMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.left * velocity * Time.deltaTime; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Obstacle")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/DinoGameStuff/SpawnObstaclesManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstaclesManager : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject obstaclePrefab;
+    public Vector2 timeToSpawnInterval;
+    private float timeToSpawn;
+    private float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTimeToSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime >= timeToSpawn)
+        {
+            SpawnObstacle();
+            currentTime = 0;
+            SetTimeToSpawn();
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        int rnd = Random.Range(1, 11);
+        if(rnd <= 5)
+        {
+            GameObject newObstacle = Instantiate(obstaclePrefab, spawnPoints[0].position, Quaternion.Euler(0,0,0));
+            int rnd0 = Random.Range(1, 11);
+            if (rnd0 > 7) newObstacle.transform.localScale = new Vector3(2, 1, 1);
+        }
+        else if(rnd > 5 && rnd <= 8)
+        {
+            GameObject newObstacle = Instantiate(obstaclePrefab, spawnPoints[1].position, Quaternion.Euler(0, 0, 0));
+            int rnd0 = Random.Range(1, 11);
+            if (rnd0 > 7) newObstacle.transform.localScale = new Vector3(2, 1, 1);
+        }
+        else
+        {
+            GameObject newObstacle = Instantiate(obstaclePrefab, spawnPoints[2].position, Quaternion.Euler(0, 0, 0));
+            int rnd0 = Random.Range(1, 11);
+            if (rnd0 > 7) newObstacle.transform.localScale = new Vector3(2, 1, 1);
+        }
+    }
+    private void SetTimeToSpawn()
+    {
+        timeToSpawn = Random.Range(timeToSpawnInterval.x, timeToSpawnInterval.y);
+    }
+
+    
+}
