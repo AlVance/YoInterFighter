9741ea9bbf703552493c56ee78d177d1b9c57176
María <kirilopez99@gmail.com>
2021-08-29 00:00:00 +0200

    CosasTW

--- Assets/DinoGameStuff/Scripts/TwitterShare.cs
+++ Assets/DinoGameStuff/Scripts/TwitterShare.cs
@@ -15,18 +15,19 @@ public class TwitterShare : MonoBehaviour
     private string tweetText;
 
     string pathName;
+    string fileName;
     private void Awake()
     {
 
-#if UNITY_ANDROID
-        pathName = "storage/emulated/0/DCIM/Screenshots/";
-#else
-        pathName = "C:/Users/" + Environment.UserName +  "/OneDrive/Imágenes" + "/YoCreatureto/";
-#endif
-        if (!System.IO.Directory.Exists(pathName))
-        {
-            System.IO.Directory.CreateDirectory(pathName);
-        }
+//#if UNITY_ANDROID
+//        pathName = "storage/emulated/0/DCIM/Screenshots/";
+//#else
+//        pathName = "C:/Users/" + Environment.UserName +  "/OneDrive/Imágenes" + "/YoCreatureto/";
+//#endif
+//        if (!System.IO.Directory.Exists(pathName))
+//        {
+//            System.IO.Directory.CreateDirectory(pathName);
+//        }
     }
     private void Start()
     {
@@ -37,16 +38,22 @@ public class TwitterShare : MonoBehaviour
     }
     public void ShareOnTwitter()
     {
+#if UNITY_ANDROID
 
-        tweetText = "¡Mi puntuación en Yo,Creatureto es de " + puntuation + " puntos!" + "\n" + "Prueba a superarme: https://kiwiteam.itch.io/yocreatureto-dinogame " + "#HoldMyBeer" + "\n" + "[Hay captura de la puntuación en tu galería :D]";
-        string fileName = "YoCreatureto_LastPuntuation.jpg";
+        //StartCoroutine(TakeScreenshotAndSave());
+        pathName = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        fileName = "/YoCreatureto_LastPuntuation.png";
+#else
+        pathName = "C:/Users/" + Environment.UserName +  "/OneDrive/Imágenes" + "/YoCreatureto/";
+       fileName = "YoCreatureto_LastPuntuation.png";
+
+#endif
+
         ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(pathName, fileName));
+        tweetText = "¡Mi puntuación en Yo,Creatureto es de " + puntuation + " puntos!" + "\n" + "Prueba a superarme: https://kiwiteam.itch.io/yocreatureto-dinogame " + "#HoldMyBeer" + "\n" + "[Inserta una captura :D]";
         Debug.Log(pathName + fileName);
-        
-        Application.OpenURL(twitterAdress + "?text=" + WWW.EscapeURL(tweetText) + /*"?media_data=" 
-           + Convert.ToBase64String(NewBehaviourScript.CapturaFotografica()) + "?media_category="  + "tweet_image" +*/ "&amp;lang=" + WWW.EscapeURL(tweetLanguage));
+        Application.OpenURL(twitterAdress + "?text=" + WWW.EscapeURL(tweetText) + "&amp;lang=" + WWW.EscapeURL(tweetLanguage));
         
     }
 
-    
 }
