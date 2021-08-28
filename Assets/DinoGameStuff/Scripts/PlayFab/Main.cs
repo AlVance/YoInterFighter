be837b62e0fc0134217184a90ba639bc5b22ef3e
Alvaro Torre Van Cutsem <alvarotvance@gmail.com>
2021-08-28 20:45:10 +0200

    Panel Input Name

--- Assets/DinoGameStuff/Scripts/PlayFab/Main.cs
+++ Assets/DinoGameStuff/Scripts/PlayFab/Main.cs
@@ -14,8 +14,10 @@ namespace Code
         [SerializeField] private Button _getLeaderboardButton;
         [SerializeField] private Button _getLeaderboardAroundPlayerButton;
         [SerializeField] private Button _getPlayerScoreButton;
-        [SerializeField] private Button _addPlayerScoreButton;
+        [SerializeField] private Button _submitNameButton;
+        [SerializeField] private InputField _nameField;
         [SerializeField] private Text _resultsText;
+        [SerializeField] private DinoMainManager _dinoMngr;
 
         private const string LeaderboardScore = "high_score";
         private const string LeaderboardName = "names";
@@ -58,7 +60,22 @@ namespace Code
             _getLeaderboardButton.onClick.AddListener(OnGetLeaderboardButtonPressed);
             _getLeaderboardAroundPlayerButton.onClick.AddListener(OnGetLeaderboardAroundPlayerButtonPressed);
             _getPlayerScoreButton.onClick.AddListener(OnGetPlayerScoreButtonPressed);
-            _addPlayerScoreButton.onClick.AddListener(OnAddPlayerScoreButtonPressed);
+            //_addPlayerScoreButton.onClick.AddListener(OnAddPlayerScoreButtonPressed);
+            _submitNameButton.onClick.AddListener(() => SubmitName(_nameField));
+        }
+
+
+        public void SubmitName(InputField namefield)
+        {
+            if (namefield.text != string.Empty)
+            {
+                _playFabLogin.SubmitNameButton(namefield);
+            }
+        }
+
+        public bool CheckName()
+        {
+            return _playFabLogin.CheckNamed();
         }
 
         private void OnAddPlayerScoreButtonPressed()
@@ -81,244 +98,8 @@ namespace Code
 
         private void OnGetLeaderboardButtonPressed()
         {
-            string name = "pepito";
-            Debug.Log(CodifyString(name));
-            Debug.Log(DecodifyInt(CodifyString(name)));
-
-            OnDisplayNameUpdate();
-
-            //_playFabGetLeaderboard.GetLeaderboardEntries(0, 5, LeaderboardScore);¡
+            _playFabGetLeaderboard.GetLeaderboardEntries(0, 5, LeaderboardScore);
 
         }
-        void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
-        {
-            Debug.Log(result);
-        }
-
-        public int CodifyString(string stringToNum)
-        {
-            int code = 0;
-            string totalCode = string.Empty;
-            char[] charArray = stringToNum.ToCharArray();
-            for (int i = 0; i < charArray.Length; i++)
-            {
-                totalCode += CharToNum(charArray[i]);
-                Debug.Log(Convert.ToInt32(totalCode));
-            }
-            code = Convert.ToInt32(totalCode);
-            return code;
-        }
-
-        public string DecodifyInt(int intToStr)
-        {
-            string encoder = string.Empty;
-
-            string lastStr;
-            string intStr = intToStr.ToString();
-            for (int i = 0; i < intStr.Length; i=i + 2)
-            {
-                lastStr = encoder.Substring(i, 2);
-                //encoder += NumToChar(Convert.ToInt32(lastStr));
-                Debug.Log(encoder);
-            }
-            return encoder;
-        }
-
-        public int CharToNum(char charToNum)
-        {
-            int num = 0;
-            int num2 = 0;
-
-            num = char.ToUpper(charToNum) - 64;
-            Debug.Log("Num Char " + num);
-            num2 = (int)charToNum % 32;
-            Debug.Log("Num Char 2 " +num2);
-            return num;
-        }
-
-
-        /*public int CharToNum(char charToNum)
-        {
-            int num = 0;
-
-            switch (charToNum)
-            {
-                case 'a':
-                    num = 10;
-                    break;
-                case 'b':
-                    num = 11;
-                    break;
-                case 'c':
-                    num = 12;
-                    break;
-                case 'd':
-                    num = 13;
-                    break;
-                case 'e':
-                    num = 14;
-                    break;
-                case 'f':
-                    num = 15;
-                    break;
-                case 'g':
-                    num = 16;
-                    break;
-                case 'h':
-                    num = 17;
-                    break;
-                case 'i':
-                    num = 18;
-                    break;
-                case 'j':
-                    num = 19;
-                    break;
-                case 'k':
-                    num = 20;
-                    break;
-                case 'l':
-                    num = 21;
-                    break;
-                case 'm':
-                    num = 22;
-                    break;
-                case 'n':
-                    num = 23;
-                    break;
-                case 'ñ':
-                    num = 24;
-                    break;
-                case 'o':
-                    num = 25;
-                    break;
-                case 'p':
-                    num = 26;
-                    break;
-                case 'q':
-                    num = 27;
-                    break;
-                case 'r':
-                    num = 28;
-                    break;
-                case 's':
-                    num = 29;
-                    break;
-                case 't':
-                    num = 30;
-                    break;
-                case 'u':
-                    num = 31;
-                    break;
-                case 'v':
-                    num = 32;
-                    break;
-                case 'w':
-                    num = 33;
-                    break;
-                case 'x':
-                    num = 34;
-                    break;
-                case 'y':
-                    num = 35;
-                    break;
-                case 'z':
-                    num = 36;
-                    break;
-            }
-            return num;
-        }
-        
-        public string NumToChar(int charToNum)
-        {
-            string str = string.Empty;
-
-            switch (charToNum)
-            {
-                case 10:
-                    str = "a";
-                    break;
-                case 11:
-                    str = "b";
-                    break;
-                case 12:
-                    str = "c";
-                    break;
-                case 13:
-                    str = "d";
-                    break;
-                case 14:
-                    str = "e";
-                    break;
-                case 15:
-                    str = "f";
-                    break;
-                case 16:
-                    str = "g";
-                    break;
-                case 17:
-                    str = "h";
-                    break;
-                case 18:
-                    str = "i";
-                    break;
-                case 19:
-                    str = "j";
-                    break;
-                case 20:
-                    str = "k";
-                    break;
-                case 21:
-                    str = "l";
-                    break;
-                case 22:
-                    str = "m";
-                    break;
-                case 23:
-                    str = "n";
-                    break;
-                case 24:
-                    str = "ñ";
-                    break;
-                case 25:
-                    str = "o";
-                    break;
-                case 26:
-                    str = "p";
-                    break;
-                case 27:
-                    str = "q";
-                    break;
-                case 28:
-                    str = "r";
-                    break;
-                case 29:
-                    str = "s";
-                    break;
-                case 30:
-                    str = "t";
-                    break;
-                case 31:
-                    str = "u";
-                    break;
-                case 32:
-                    str = "v";
-                    break;
-                case 33:
-                    str = "w";
-                    break;
-                case 34:
-                    str = "x";
-                    break;
-                case 35:
-                    str = "y";
-                    break;
-                case 36:
-                    str = "z";
-                    break;
-            }
-            return str;
-        }
-
-        */
     }
 }
--- Assets/DinoGameStuff/Scripts/PlayFab/PlayFabLogin.cs
+++ Assets/DinoGameStuff/Scripts/PlayFab/PlayFabLogin.cs
@@ -2,12 +2,14 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code
 {
     public class PlayFabLogin
     {
         public event Action<string> OnSuccess;
+        public bool named;
         public void Login()
         {
             if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId)) 
@@ -20,15 +22,61 @@ namespace Code
                 PlayFabSettings.staticSettings.TitleId = "TU TITLE ID";
             }
 
-            var request = new LoginWithCustomIDRequest {CustomId = "GettingStartedGuide3", CreateAccount = true};
+#if UNITY_ANDROID
+            var androidRequest = new LoginWithAndroidDeviceIDRequest
+            {
+                AndroidDeviceId = SystemInfo.deviceUniqueIdentifier, CreateAccount = true
+            };
+            PlayFabClientAPI.LoginWithAndroidDeviceID(androidRequest, OnLoginSuccess, OnLoginFailure);
+#elif UNITY_IOS
+            var iosRequest = new LoginWithIOSDeviceIDRequest
+            {
+                DeviceId = SystemInfo.deviceUniqueIdentifier, CreateAccount = true
+            };
+            PlayFabClientAPI.LoginWithIOSDeviceID(iosRequest, OnLoginSuccess, OnLoginFailure);
+#else
+            var request = new LoginWithCustomIDRequest 
+            { 
+                CustomId = "PlayerDinoGame", CreateAccount = true, InfoRequestParameters = new GetPlayerCombinedInfoRequestParams {GetPlayerProfile = true } 
+            };
             PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+#endif
+
             // SystemInfo.deviceUniqueIdentifier;
         }
 
         private void OnLoginSuccess(LoginResult result)
         {
             Debug.Log("Login");
-            OnSuccess?.Invoke(result.PlayFabId);
+            //OnSuccess?.Invoke(result.PlayFabId);
+
+            string name = null;
+            if(result.InfoResultPayload.PlayerProfile != null)
+            {
+                name = result.InfoResultPayload.PlayerProfile.DisplayName;
+                if(name == null) named = true; else  named = false;
+                Debug.Log(name);
+            }
+        }
+
+        public bool CheckNamed()
+        {
+            return named;
+        }
+
+        public void SubmitNameButton(InputField nameField)
+        {
+            var request = new UpdateUserTitleDisplayNameRequest
+            {
+                DisplayName = nameField.text,
+            };
+            PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnLoginFailure);
+        }
+
+        void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
+        {
+            Debug.Log("Update disaple name!");
+
         }
 
         private void OnLoginFailure(PlayFabError error)
